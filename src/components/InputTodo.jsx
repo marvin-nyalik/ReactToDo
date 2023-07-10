71c0b35ce56856234a2c6a0848eb8261221c32fc
MARVIN <o.silverygold@gmail.com>
2023-07-10 14:58:09 +0300

    React tutorial project

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,14 @@
+const InputTodo = () => {
+    return (
+      <>
+    <form className="form-container">
+      <input className="input-text"
+      />
+      <button className="input-submit">Submit</button>
+    </form>
+    <span className="submit-warning"></span>
+  </>
+    );
+  };
+  export default InputTodo;
+  
