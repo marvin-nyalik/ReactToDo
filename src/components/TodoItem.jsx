134e52ad6c43d2727a5f3927d861b93dfc70a3a5
MARVIN <o.silverygold@gmail.com>
2023-07-11 10:32:14 +0300

    Add my react tutorial project

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,7 @@
-const TodoItem = ({ itemProp, setTodos }) => {  
+import { useState } from 'react';
+
+const TodoItem = ({ itemProp, setTodos, delTodo }) => {  
+  const [editing, setEditing] = useState(false);
   const handleChange = (id) => {
     setTodos((prevState) =>
     prevState.map((todo) => {
@@ -12,11 +15,21 @@ const TodoItem = ({ itemProp, setTodos }) => {
     })
   );  };
 
-    return <li><input type="checkbox"
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+    return (<li>
+      <div>
+      <input type="checkbox"
     checked = {itemProp.completed} 
     onChange={() => handleChange(itemProp.id)}
     />
-     {itemProp.title}</li>;
+     {itemProp.title}
+     <button onClick={handleEditing}>Edit</button>
+     <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+      </div>
+    </li>);
   };
   export default TodoItem;
   
